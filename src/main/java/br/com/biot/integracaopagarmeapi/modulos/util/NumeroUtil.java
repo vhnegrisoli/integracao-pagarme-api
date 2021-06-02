f90041284e770c4db7377ab14c681232928d7666
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2021-06-01 21:15:22 -0300

    Adiciona valor do pagamento no retorno da transação

--- src/main/java/br/com/biot/integracaopagarmeapi/modulos/util/NumeroUtil.java
+++ src/main/java/br/com/biot/integracaopagarmeapi/modulos/util/NumeroUtil.java
@@ -1,6 +1,7 @@
 package br.com.biot.integracaopagarmeapi.modulos.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -11,6 +12,7 @@ public class NumeroUtil {
     private static final Integer UMA_CASA_DECIMAL = 1;
     private static final Integer DUAS_CASAS_DECIMAIS = 2;
     private static final String PONTO_DECIMAL = ".";
+    private static final String PREFIXO_DUAS_CASAS = ".00";
     private static final Integer INICIO_NUMERO = 0;
 
     public static BigDecimal converterParaDuasCasasDecimais(Double numero) {
@@ -21,19 +23,26 @@ public class NumeroUtil {
 
     public static BigDecimal tratarValorPagamento(BigDecimal valorPagamento) {
         try {
-            var valorString = valorPagamento.toString();
-            if (valorString.length() == UMA_CASA_DECIMAL) {
-                valorString = valorString.concat(String.valueOf(INICIO_NUMERO));
-            }
-            var tamanhoNumero = valorString.length();
+            var valorPagamentoString = tratarStringNumericaInicial(valorPagamento.toString());
+            var tamanhoNumero = valorPagamentoString.length();
             var tamanhoInteiro = tamanhoNumero - DUAS_CASAS_DECIMAIS;
-            var valorDecimal = valorString.substring(tamanhoInteiro);
-            var valorInteiro = valorString.substring(INICIO_NUMERO, tamanhoInteiro);
+            var valorDecimal = valorPagamentoString.substring(tamanhoInteiro);
+            var valorInteiro = valorPagamentoString.substring(INICIO_NUMERO, tamanhoInteiro);
             var numeroCompletoComDuasCasasDecimais = valorInteiro.concat(PONTO_DECIMAL).concat(valorDecimal);
             return converterParaDuasCasasDecimais(Double.parseDouble(numeroCompletoComDuasCasasDecimais));
         } catch (Exception ex) {
             log.error("Erro ao tentar converter o número para duas casas dentro do número.");
             return BigDecimal.ZERO;
         }
+    }
+
+    private static String tratarStringNumericaInicial(String valor) {
+        if (valor.contains(PREFIXO_DUAS_CASAS)) {
+            valor = valor.replace(PREFIXO_DUAS_CASAS, Strings.EMPTY);
+        }
+        if (valor.length() == UMA_CASA_DECIMAL) {
+            valor = valor.concat(String.valueOf(INICIO_NUMERO));
+        }
+        return valor;
     }
 }
